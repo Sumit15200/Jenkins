8a5322b2138d48b04ec4d1cccbcf1e44048c8ae2
sumit15200 <sumithanjage2000@gmail.com>
2023-11-21 15:18:09 +0530

    added next process

--- JavaApp.java
+++ JavaApp.java
@@ -2,5 +2,6 @@ public class JavaApp{
     public static void main(String[] args)
     {
         System.out.println("Hello From Java");
+        System.out.println("Automatic build using jenkins");
     }
 }
