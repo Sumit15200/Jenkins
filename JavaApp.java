59509b6039163e3805c560e9a0f17d5cd83468cd
sumit15200 <sumithanjage2000@gmail.com>
2023-11-21 15:45:57 +0530

    created new

--- JavaApp.java
+++ JavaApp.java
@@ -1,7 +1,7 @@
 public class JavaApp{
     public static void main(String[] args)
     {
-        System.out.println("Hello From Java");
+        System.out.println("Hello From Java2");
         System.out.println("Automatic build using jenkins");
     }
 }
